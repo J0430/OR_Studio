cb44f1b09be23c054ee022d36f2a2c74834b227e
manumurillo0430 <manurillo33@gmail.com>
2025-02-11 21:55:57 +0000

    Save local changes before merging

---
+++ client/globals/globals.js
@@ -0,0 +1,42 @@
+// globals.js
+export const HomePictures = [
+  "/assets/home/Home Pictures/Front 3.jpg",
+  "/assets/home/Home Pictures/Front Page 1.jpg",
+  "/assets/home/Home Pictures/Front Page 2.jpg",
+  "/assets/home/Home Pictures/Front Page 5.jpg",
+];
+export const Logos = [
+  "/assets/logos/OR Studio.png",
+  "/assets/logos/OR.png",
+  "/assets/logos/OR_white.png",
+  "/assets/logos/OR.svg",
+];
+export const Even_Yehuda = [
+  "/assets/home/Even_Yehuda/4.png",
+  "/assets/home/Even_Yehuda/5.png",
+  "/assets/home/Even_Yehuda/7.png",
+  "/assets/home/Even_Yehuda/8.png",
+];
+
+export const Hevron8 = [
+  "/assets/home/Hevron 8 Penthouse Rooftop/Hevron_003-View2_PS.jpg",
+  "/assets/home/Hevron 8 Penthouse Rooftop/Hevron_003-View3_PS.jpg",
+  "/assets/home/Hevron 8 Penthouse Rooftop/Hevron_003-View4_PS.jpg",
+  "/assets/home/Hevron 8 Penthouse Rooftop/Hevron_003-View5_PS.jpg",
+];
+
+export const City69 = [
+  "/assets/home/City69/13.png",
+  "/assets/home/City69/10.png",
+  "/assets/home/City69/11.png",
+  "/assets/home/City69/12.png",
+];
+
+export const categories = [
+  "Categories",
+  "Residential",
+  "Comercial",
+  "Urban Planning",
+  "Office",
+  "Animation",
+];
