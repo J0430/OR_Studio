360d8e5f5c1e60c1bad7a74df0df12912774006e
manumurillo0430 <manurillo33@gmail.com>
2025-02-15 20:07:11 +0000

    Adding _redirects

--- client/app/contact/page.jsx
+++ client/app/contact/page.jsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 import Image from "next/image";
 import TypingEffect from "@components/preloaders/MainPreloader/_Mainpreloader/TypingEffect/TypingEffect";
 import { Logos } from "@utils/api";
-import styles from "@components/preloaders/MainPreloader/MainPreloader.module.scss";
+import styles from "@components/preloaders/MainPreloader/_Mainpreloader/MainPreloader.module.scss";
 
 const ContactPage = () => {
   return (
