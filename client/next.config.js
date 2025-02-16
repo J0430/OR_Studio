29a8d46dd183416c136eb3ed4b843ea642d631f1
manumurillo0430 <manurillo33@gmail.com>
2025-02-16 01:42:35 +0000

    Fixes

--- client/next.config.js
+++ client/next.config.js
@@ -3,23 +3,20 @@ const path = require("path");
 
 const nextConfig = {
   reactStrictMode: true,
-
-  // Enables static site generation/export
   output: "export",
-  sassOptions: {
-    // Include the styles directory for SCSS/SASS resolution
-    includePaths: [path.join(__dirname, "styles")],
-  },
-
   images: {
-    // Required for static export: disables Next.js image optimization
     unoptimized: true,
   },
-
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/,
-      use: ["@svgr/webpack"], // Allows importing SVGs as React components
+      use: ["@svgr/webpack"],
     });
     return config;
   },
