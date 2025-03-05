70a4d8e03cb3d5ef76c8aed9ba1ea15b3b60534f
manumurillo0430 <manurillo33@gmail.com>
2025-03-05 13:14:20 +0000

    Contact Page

--- client/components/sections/contact/ContactHeader.jsx
+++ client/components/sections/contact/ContactHeader.jsx
@@ -1,7 +1,12 @@
+import { useMediaQuery } from "react-responsive";
 import styles from "@components/sections/contact/ContactHeader.module.scss";
 const ContactHeader = ({ title, content }) => {
+  const isMobile = useMediaQuery({ maxWidth: 768 });
   return (
-    <div className={styles.contactContainer}>
+    <div
+      className={
+        isMobile ? styles.mobileContactContainer : styles.contactContainer
+      }>
       <h2 className>{title}</h2>
       {content.map((paragraph, index) => (
         <p key={index}>{paragraph}</p>
