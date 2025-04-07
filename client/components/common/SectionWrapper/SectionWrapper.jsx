1a942a405602ac025e73742781ed5e9f0dffd3e5
manumurillo0430 <manurillo33@gmail.com>
2025-04-07 20:58:22 +0100

    Code improvements

--- client/components/common/SectionWrapper/SectionWrapper.jsx
+++ client/components/common/SectionWrapper/SectionWrapper.jsx
@@ -3,18 +3,18 @@ import { useInView } from "react-intersection-observer";
 
 const SectionWrapper = ({ id, children }) => {
   const [ref, inView] = useInView({
-    triggerOnce: true, // Animate only once per section
-    threshold: 0.3, // 30% of section in viewport to trigger
+    triggerOnce: true,
+    threshold: 0, // 30% of section in viewport to trigger
   });
 
   return (
     <motion.section
       id={id}
       ref={ref}
-      initial={{ opacity: 0, y: 80 }} // Start hidden and below
+      initial={{ opacity: 0, y: 100 }} // Start hidden and below
       animate={inView ? { opacity: 1, y: 0 } : {}} // Animate when in view
       transition={{ duration: 0.8, ease: "easeOut" }} // Smooth transition
-    >
+      style={{ height: "100dvh" }}>
       {children}
     </motion.section>
   );
