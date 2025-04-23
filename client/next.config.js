86d86be50a39e3584ecfaf7b199068ce661fc8f9
manumurillo0430 <manurillo33@gmail.com>
2025-04-23 21:16:45 +0100

    Code improvements

--- client/next.config.js
+++ client/next.config.js
@@ -3,6 +3,7 @@ const path = require("path");
 const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
+  output: "export",
   experimental: {
     esmExternals: true,
     reactRefresh: false,
