67cba0d5bc493c1169d14f54efbccae598a9952e
J0430 <jopulido@outlook.com>
2025-04-23 21:37:33 +0100

    Update next.config.js

--- client/next.config.js
+++ client/next.config.js
@@ -3,43 +3,44 @@ const path = require("path");
 const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
+  output: "export",
   experimental: {
     esmExternals: true,
-    reactRefresh: false,
   },
   reactStrictMode: true,
   images: {
     domains: ["monumental-kleicha-0d19a2.netlify.app"],
     unoptimized: true,
   },
-
   assetPrefix: isProd ? process.env.NEXT_PUBLIC_ASSET_PREFIX || "" : "",
-
   sassOptions: {
     includePaths: [path.join(process.cwd(), "styles")],
   },
-
   eslint: {
     ignoreDuringBuilds: process.env.NODE_ENV === "production",
   },
-
   typescript: {
     ignoreBuildErrors: true,
   },
-
   webpack: (config) => {
-    config.module.rules.push({
-      test: /\.(png|jpe?g|gif|svg)$/i,
-      use: [
-        "@svgr/webpack",
-        {
-          loader: "file-loader",
-          options: {
-            name: "[path][name].[ext]",
+    config.module.rules.push(
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        use: [
+          "@svgr/webpack",
+          {
+            loader: "file-loader",
+            options: {
+              name: "[path][name].[ext]",
+            },
           },
-        },
-      ],
-    });
+        ],
+      },
+      {
+        test: /\.md$/,
+        use: "ignore-loader",
+      }
+    );
 
     config.resolve.fallback = {
       path: require.resolve("path-browserify"),
@@ -48,3 +49,5 @@ const nextConfig = {
     return config;
   },
 };
+
+module.exports = nextConfig;
