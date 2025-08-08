8d85af4a27766908a553f4395e9d2ca4286b33c5
J0430 <jopulido@outlook.com>
2025-08-08 06:39:53 +0100

    Update SectionWrapper.jsx

--- client/components/common/SectionWrapper/SectionWrapper.jsx
+++ client/components/common/SectionWrapper/SectionWrapper.jsx
@@ -4,7 +4,7 @@ import { useInView } from "react-intersection-observer";
 const SectionWrapper = ({ id, children }) => {
   const [ref, inView] = useInView({
     triggerOnce: true,
-    threshold: 1,
+    threshold: 0.3, // 30% in view is enough
   });
 
   return (
@@ -12,8 +12,9 @@ const SectionWrapper = ({ id, children }) => {
       id={id}
       ref={ref}
       initial={{ opacity: 0, y: 100 }}
-      animate={inView ? { opacity: 1, y: 100 } : { opacity: 1, y: 100 }}
-      transition={{ duration: 0.1, ease: "easeOut" }}>
+      animate={inView ? { opacity: 1, y: 0 } : {}} // ✅ Proper reveal
+      transition={{ duration: 0.8, ease: "easeOut" }}
+    >
       {children}
     </motion.section>
   );
